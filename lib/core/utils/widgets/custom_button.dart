aab9119ea4b413f765f9351a5dee900ba9872f3e
hussein_salah <husseinsalah2528@gmail.com>
2024-08-29 02:25:06 +0300

    edit custombutton

--- lib/core/utils/widgets/custom_button.dart
+++ lib/core/utils/widgets/custom_button.dart
@@ -2,9 +2,11 @@ import 'package:checkout_payment/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  CustomButton({super.key, this.onTap, required this.text});
+  CustomButton(
+      {super.key, this.onTap, required this.text, this.isLoading = false});
   void Function()? onTap;
   String text;
+  bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -16,10 +18,12 @@ class CustomButton extends StatelessWidget {
             color: const Color(0xff34A853),
             borderRadius: BorderRadius.circular(15)),
         child: Center(
-          child: Text(
-            text,
-            style: Styles.style22,
-          ),
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : Text(
+                  text,
+                  style: Styles.style22,
+                ),
         ),
       ),
     );
